bf5049026b72a70ec157277b59904fe278c920d4
Daniel Getz <dan@getzit.net>
2024-02-11 01:38:54 -0500

    turn off blinking from clicking between items

--- app/src/main/java/net/getzit/repti/MainActivity.kt
+++ app/src/main/java/net/getzit/repti/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -160,7 +161,10 @@ fun MainScreen(tasks: List<Task>) {
             Box(
                 modifier = Modifier
                     .weight(1f)
-                    .clickable(onClick = clearSelectedId)
+                    .clickable(
+                        onClick = clearSelectedId,
+                        indication = null,
+                        interactionSource = remember { MutableInteractionSource() })
             ) {
                 LazyColumn(
                     state = listState,
